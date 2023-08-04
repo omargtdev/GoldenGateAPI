d8fd0ee798fc1ac5f251ee37b988727d7fa44dfe
omargtdev <omargtdev@gmail.com>
2023-08-04 01:31:59 -0500

    data(context): Context db and dummy data added

---
+++ GoldenGateAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GoldenGateAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldenGateAPI.Data;
+
+public class AppDbContext : IdentityDbContext<User> // <User> User class represented to use in DB
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    // Create all tables in the DB defined by the context with his models
+/*     protected override void OnModelCreating(ModelBuilder builder) 
+    {
+        base.OnModelCreating(builder);
+    } */
+
+    public DbSet<Property> Properties { get; set; }
+
+}
---
+++ GoldenGateAPI/Data/LoadDatabase.cs
@@ -0,0 +1,74 @@
+﻿using GoldenGateAPI.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GoldenGateAPI.Data;
+
+public class LoadDatabase
+{
+
+    public static async Task FirstInsertData(AppDbContext context, UserManager<User> userManager) 
+    {
+
+        Task? userCreation = null;
+        Task? propertiesCreation = null;
+
+        if(!userManager.Users.Any())
+        {
+            var user = new User {
+                Name = "Omar",
+                LastName = "Gutierrez",
+                Email = "omargtdev@gmail.com",
+                UserName = "omargtdev",
+                PhoneNumber = "987654321"
+            };
+
+            userCreation = userManager.CreateAsync(user, "Passw0rd$123");
+        }
+
+        if(!context.Properties.Any())
+        {
+            propertiesCreation = context.Properties.AddRangeAsync(
+                new Property {
+                    Name = "Casa de Playa",
+                    Direction = "Av. El sol 123",
+                    Price = 45000.50m
+                },
+                new Property {
+                    Name = "Casa de Campo",
+                    Direction = "Av. La Luna 152",
+                    Price = 35000.50m
+                },
+                new Property {
+                    Name = "Casa de Invierno",
+                    Direction = "Av. La roca 321",
+                    Price = 20000.50m
+                },
+                new Property {
+                    Name = "Casa Rustica",
+                    Direction = "Av. Los pitucos 023",
+                    Price = 60500.50m
+                },
+                new Property {
+                    Name = "Casa de los Simpson",
+                    Direction = "Av. Siempre Viva 123",
+                    Price = 142000m
+                },
+                new Property {
+                    Name = "Casa de Omar",
+                    Direction = "Av. Some street 123",
+                    Price = 1000m
+                }
+            );
+
+        }
+
+        if(userCreation is not null)
+            await userCreation; 
+
+        if(propertiesCreation is not null)
+            await propertiesCreation;
+
+        context.SaveChanges();
+    }
+
+}
